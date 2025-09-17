f88d1ce8ee26e8c532e14f088ed470baf4c29cdc
hainho <hainho9704@gmail.com>
2025-09-18 05:16:23 +0900

    feat: commerce-batch app init

---
+++ apps/commerce-batch/build.gradle.kts
@@ -0,0 +1,27 @@
+dependencies {
+    // add-ons
+    implementation(project(":apps:commerce-api"))
+    implementation(project(":apps:commerce-streamer"))
+    implementation(project(":modules:jpa"))
+    implementation(project(":modules:redis"))
+    implementation(project(":modules:kafka"))
+    implementation(project(":supports:jackson"))
+    implementation(project(":supports:logging"))
+    implementation(project(":supports:monitoring"))
+
+    // web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    // querydsl
+    annotationProcessor("com.querydsl:querydsl-apt::jakarta")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+
+    // test-fixtures
+    testImplementation(testFixtures(project(":apps:commerce-api")))
+    testImplementation(testFixtures(project(":apps:commerce-streamer")))
+    testImplementation(testFixtures(project(":modules:jpa")))
+    testImplementation(testFixtures(project(":modules:redis")))
+    testImplementation(testFixtures(project(":modules:kafka")))
+}
